427142f7799480837feaf0cbe9faa504f868b91f
raoole20 <espinaraul8@gmail.com>
2024-11-14 22:12:08 -0400

    add rese~na page

--- frontend/src/app/dashboard/report/new/page.tsx
+++ frontend/src/app/dashboard/report/new/page.tsx
@@ -1,30 +1,22 @@
-import { ReportsForm } from "@/components/forms/reports";
-import {
-  Accordion,
-  AccordionItem,
-  AccordionTrigger,
-  AccordionContent,
-} from "@/components/ui/accordion";
 import React from "react";
+import ReportFormPage from "./components/Form";
+import { Button } from "@/components/ui/button";
 
 export default function page() {
   return (
     <div className="space-y-5">
-      <div>
-        <h1 className="text-2xl">Crear un nueva nueva Resena</h1>
-        <p className="text-sm mt-2">
-          Por favor, llena el siguiente formulario para crear un nuevo reporte.
-        </p>
+      <div className="flex justify-between items-center">
+        <div>
+          <h1 className="text-2xl">Crear un nueva nueva Resena</h1>
+          <p className="text-sm mt-2">
+            Por favor, llena el siguiente formulario para crear un nuevo reporte.
+          </p>
+        </div>
+
+        <Button className="bg-primary">Crear Resena</Button>
       </div>
 
-      <Accordion type="multiple" className="w-full">
-        <AccordionItem value="item-1" className="p-4 border rounded-lg" defaultChecked>
-          <AccordionTrigger className="hover:no-underline">Datos Basicos del Ciudadano</AccordionTrigger>
-          <AccordionContent>
-            <ReportsForm />
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+      <ReportFormPage />
     </div>
   );
 }
